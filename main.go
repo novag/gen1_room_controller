ed40dccdcd5736efd092062c19125c49bc63801f
novag <git@hendrik.li>
2019-12-22 18:33:23 +0100

    Add ssh control

--- main.go
+++ main.go
@@ -20,11 +20,20 @@ const (
     rockroboBasePath = "/mnt/data/rockrobo/"
     roomControllerBasePath = "/mnt/data/room_controller/"
     fullMapPath = roomControllerBasePath + "full/"
+    sshPrivateKeyPath = "/root/.ssh/id_ed25519"
+    sshPublicKeyPath = "/root/.ssh/id_ed25519.pub"
+    sshKnownHostsPath = "/root/.ssh/known_hosts"
 )
 
 type Coordinates struct {
     X int
     Y int
+}
+
+type RemoteHost struct {
+    Address     string
+    Port        string
+    FetchKey    bool
 }
 
 func getMiioToken() (string, error) {
@@ -157,6 +166,56 @@ var goToMsgRcvd = func(client mqtt.Client, message mqtt.Message) {
     client.Publish(message.Topic() + "/status", 0, false, "Success")
 }
 
+var sshPubKeyMsgRcvd = func(client mqtt.Client, message mqtt.Message) {
+    os.Remove(sshPrivateKeyPath)
+    os.Remove(sshPublicKeyPath)
+    cmd := exec.Command("ssh-keygen", "-t", "ed25519", "-f", sshPrivateKeyPath, "-C", "vacuum_1", "-q", "-N", "")
+    if err := cmd.Run(); err != nil {
+        client.Publish(message.Topic() + "/status", 0, false, err.Error())
+        return
+    }
+
+    data, err := ioutil.ReadFile(sshPublicKeyPath)
+    if err != nil {
+        client.Publish(message.Topic() + "/status", 0, false, err.Error())
+        return
+    }
+
+    client.Publish(message.Topic() + "/status", 0, false, data)
+}
+
+var sshTunnelMsgRcvd = func(client mqtt.Client, message mqtt.Message) {
+    var remoteHost RemoteHost
+
+    if err := json.Unmarshal(message.Payload(), &remoteHost); err != nil {
+        client.Publish(message.Topic() + "/status", 0, false, err.Error())
+        return
+    }
+
+    if remoteHost.FetchKey {
+        file, err := os.OpenFile(sshKnownHostsPath, os.O_CREATE|os.O_WRONLY, 0644)
+        if err != nil {
+            client.Publish(message.Topic() + "/status", 0, false, err.Error())
+            return
+        }
+        defer file.Close()
+
+        cmd := exec.Command("ssh-keyscan", "-H", "-p", remoteHost.Port, remoteHost.Address)
+        cmd.Stdout = file
+        if err := cmd.Run(); err != nil {
+            client.Publish(message.Topic() + "/status", 0, false, err.Error())
+            return
+        }
+    }
+
+    cmd := exec.Command("ssh", "-f", "-N", "-T", "-R52222:localhost:22", mqttUsername + "@" + remoteHost.Address, "-p", remoteHost.Port)
+    if err := cmd.Run(); err != nil {
+        client.Publish(message.Topic() + "/status", 0, false, err.Error())
+    }
+
+    client.Publish(message.Topic() + "/status", 0, false, "Success")
+}
+
 func main() {
     signalChannel := make(chan os.Signal, 1)
     signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
@@ -179,6 +238,14 @@ func main() {
     }
 
     if token := client.Subscribe("devices/vacuum/1/goto", 0, goToMsgRcvd); token.Wait() && token.Error() != nil {
+        fmt.Println(token.Error())
+    }
+
+    if token := client.Subscribe("devices/vacuum/1/ssh/pubkey", 0, sshPubKeyMsgRcvd); token.Wait() && token.Error() != nil {
+        fmt.Println(token.Error())
+    }
+
+    if token := client.Subscribe("devices/vacuum/1/ssh/tunnel", 0, sshTunnelMsgRcvd); token.Wait() && token.Error() != nil {
         fmt.Println(token.Error())
     }
 
